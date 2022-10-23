8fd9cb439aa72a31523713f71cb6dcde860e9ab5
Hornet004 <davidboostupnow@gmail.com>
2022-10-23 22:32:29 +0100

    modified bundle

--- bundle.js
+++ bundle.js
@@ -9,39 +9,33 @@ module.exports = rangeSlider
 
 function get_theme() {
     return`
+    :host(.container){
+        background: red;
+    }
     .range_class{
         width: 100%;
-    }
-    .date_class{
-        padding: 12px;
-        width: 48%;
-        background-color: #eaecff;
-        height: 4vh;
-        border-radius: 4px;
-        border: none;
-    }
-    .date_class:hover{
-        background-color: #65abfd;  
     }
     `
 }
 
 function rangeSlider() {
     const el = document.createElement('div')
+    el.classList.add('container')
     const shadow = el.attachShadow({mode: 'closed'})
 
+    // Initialize the style  and get the style theme    
     const style = document.createElement('style')
     style.textContent = get_theme()
 
+    // Input component is created beneath this line
     const input = document.createElement('input')
-    input.classList =('range_class')
     input.type = 'range' 
+    input.classList.add('range_class')
+    
 
-    const picker = document.createElement('input')
-    picker.classList =('date_class')
-    picker.type = 'date'
+   
 
-    shadow.append(input, style, picker)
+    shadow.append(input, style)
 
     return el
 }
