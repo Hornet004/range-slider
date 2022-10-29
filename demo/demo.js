7274d0182334f5d1a983a574b887306321f9fb29
Hornet004 <davidboostupnow@gmail.com>
2022-10-29 22:28:27 +0100

    update demo js

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,19 @@
 const rangeSlider = require('..')
 
 const range = rangeSlider()
+document.body.innerHTML = `<h1>Range Slider</h1>`
+
+const main = document.createElement('div')
+main.classList.add('demo')
+
+const style = document.createElement('style')
+style.textContent = `
+.demo{
+    border: 1px solid red;
+    padding: 25px;
+}
+
+`
+main.append(range)
   
-document.body.append(range)
+document.body.append(main, style)
